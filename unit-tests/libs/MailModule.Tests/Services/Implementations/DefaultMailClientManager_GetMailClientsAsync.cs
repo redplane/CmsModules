4c965961089a692a2da614a31d512bae34fc502b
Mai Nguyen Thi <Mai.Nguyen-Thi@partners.tecalliance.net>
2021-06-06 01:02:08 +0700

    Added unit test project.

---
+++ unit-tests/libs/MailModule.Tests/Services/Implementations/DefaultMailClientManager_GetMailClientsAsync.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailModule.Tests.Services.Implementations
+{
+    public class DefaultMailClientManager_GetMailClientsAsync
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ unit-tests/libs/MailModule.Tests/Services/Implementations/DefaultMailClientManager_MarkMailClientAsActiveAsync.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailModule.Tests.Services.Implementations
+{
+    class DefaultMailClientManager_MarkMailClientAsActiveAsync
+    {
+    }
+}
